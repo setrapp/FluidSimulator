97a462ce74d3647c7e860a791288285386a9e0b4
Sam Trapp <trapp.s.e@gmail.com>
2017-10-29 12:04:12 -0600

    WIP FluidRider applys force to rigidbody based on fluid sample.

--- Assets/Scripts/FluidRider.cs
+++ Assets/Scripts/FluidRider.cs
@@ -9,10 +9,14 @@ public class FluidRider : MonoBehaviour
 	public Vector3 velocity = Vector3.zero;
 	[Range(0, 1)]
 	public float maxSpeedUtilization = 1;
+	[Range(0, 10)]
+	public float dragFactor = 0.5f;
 	new public Collider collider;
 
 	// TODO update this based on which FluidCollider is hit (OnCollisionEnter will not work if it requires a non-kinematic rigidbody)
 	public FluidSampler sampler;
+
+	Vector3 cellVelocity;
 
 	[HideInInspector]
 	public bool is2D = true;
@@ -33,8 +37,8 @@ public class FluidRider : MonoBehaviour
 			return;
 		}
 
-		Vector3 cellVelocity;
-		if (collider as SphereCollider != null)
+		//Vector3 cellVelocity;
+		if (false)//collider as SphereCollider != null)
 		{
 			cellVelocity = sampler.SampleCircle(transform.position, ((SphereCollider)collider).radius).velocity;
 		}
@@ -44,15 +48,15 @@ public class FluidRider : MonoBehaviour
 		}
 
 		CellParameters cellParameters = sampler.Simulator.cellParameters;
-		float maxSpeed = (cellParameters.cellMaxSpeed / cellParameters.cellSize) * maxSpeedUtilization;
-		Debug.Log(maxSpeed);
-		if (mimicFluidVelocity)
+		float maxSpeed = ((cellParameters.cellMaxSpeed / cellParameters.cellSize) * maxSpeedUtilization) / Time.deltaTime;
+
+		/*if (mimicFluidVelocity)
 		{
 			velocity = cellVelocity / mass;
 		}
 		else
 		{
-			velocity += ((cellVelocity / mass) * Time.deltaTime);
+			velocity += ((cellVelocity / mass));// * Time.deltaTime);
 		}
 		velocity.z = is2D ? 0 : velocity.z;
 		if(maxSpeed >= 0 && velocity.sqrMagnitude > (maxSpeed * maxSpeed))
@@ -61,6 +65,46 @@ public class FluidRider : MonoBehaviour
 		}
 
 		// Time.deltaTime is not used here because it not used in the fluid simulation.
-		transform.position += velocity;
+		//transform.position += velocity;
+
+		Vector3 drag = velocity * dragFactor;
+		velocity -= drag * Time.deltaTime;*/
+
+		velocity = cellVelocity / Time.deltaTime;
+		
+		var body = GetComponent<Rigidbody>();
+		// TODO Not even close..
+		// TODO Do not need mass or drag if using rigidbody
+		var fluidContribution = 1;// velocity.magnitude / maxSpeed;
+		var bodyContribution = Mathf.Clamp(body.velocity.magnitude / Mathf.Max(velocity.magnitude, 0.001f), 0, 1);//Mathf.Max(body.velocity.magnitude / maxSpeed, 0.001f);
+		Debug.Log(bodyContribution);
+		body.AddForceAtPosition(((velocity - body.velocity) * (1 - bodyContribution)) / Time.deltaTime, transform.position);
+
+		//sampler.Simulator.GetComponent<FluidCollider>().AddExternal(transform.position, 0, 0, velocity / Time.deltaTime, 2);
+
+
+
+		//body.velocity = velocity / Time.deltaTime;// cellVelocity / Time.deltaTime;
+
+		if (maxSpeed >= 0 && body.velocity.sqrMagnitude > (maxSpeed * maxSpeed))
+		{
+			body.velocity = body.velocity.normalized * maxSpeed;
+		}
+
 	}
+
+	/*void OnCollisionEnter(Collision collision)
+	{
+		var body = GetComponent<Rigidbody>();
+		body.velocity = collision.impulse.normalized * velocity.magnitude;
+	}*/
+
+	/*void OnDrawGizmos()
+	{
+		Vector3 linePos = transform.position + new Vector3(0, 0, -1);
+		Gizmos.color = Color.white;
+		Gizmos.DrawLine(linePos, linePos + cellVelocity / Time.deltaTime);
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(linePos, linePos + GetComponent<Rigidbody>().velocity);
+	}*/
 }
